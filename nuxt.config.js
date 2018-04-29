ebf740f256ff44cd9f97f62e5a712e39a2d74560
Thinktech <dev@thinktech.sn>
2018-04-29 20:25:00 +0000

    Theme color

--- nuxt.config.js
+++ nuxt.config.js
@@ -17,7 +17,7 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { name: 'theme-color', content: '#111' },
+      { name: 'theme-color', content: '#b68434' },
       {
         hid: 'description',
         name: 'description',
